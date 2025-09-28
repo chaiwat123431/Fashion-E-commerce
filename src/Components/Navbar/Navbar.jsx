2ef3197fb2a51b3a05cd848010a75b00c67d7fb7
ChaiwatAikaew <chaiwatnba@gmail.com>
2025-09-28 18:28:45 -0400

    Style top navigation bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 
 const Navbar = () => {
+  const [menu,setMenu] = useState("shop");
   return (
     <div className='navbar'>
         <div className='nav-logo'>
@@ -12,14 +13,15 @@ const Navbar = () => {
 
         </div>
         <ul className='nav-menu'>
-            <li>Shop</li>
-            <li>Men</li>
-            <li>Women</li>
-            <li>Kids</li>
+            <li onClick={()=>{setMenu("shop")}}>Shop{menu === "shop"?<h/>:<></>}</li>
+            <li onClick={()=>{setMenu("mens")}}>Men</li>
+            <li onClick={()=>{setMenu("womens")}}>Women</li>
+            <li onClick={()=>{setMenu("kids")}}>Kids</li>
         </ul>
         <div className='nav-login-cart'>
             <button>Login</button>
             <img src={cart_icon} alt=""/>
+            <div className='nav-cart-count'>0</div>
         </div>
     </div>
   )
