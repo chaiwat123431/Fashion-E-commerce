89b73de832eb1bc13109590c55506a7ef3382ff0
ChaiwatAikaew <chaiwatnba@gmail.com>
2025-09-29 19:51:27 -0400

    Add pages and setup fonts

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   const [menu,setMenu] = useState("shop");
@@ -13,10 +14,10 @@ const Navbar = () => {
 
         </div>
         <ul className='nav-menu'>
-            <li onClick={()=>{setMenu("shop")}}>Shop{menu === "shop"?<h/>:<></>}</li>
-            <li onClick={()=>{setMenu("mens")}}>Men</li>
-            <li onClick={()=>{setMenu("womens")}}>Women</li>
-            <li onClick={()=>{setMenu("kids")}}>Kids</li>
+            <li onClick={()=>{setMenu("shop")}}><Link to="">Shop</Link>{menu === "shop"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("mens")}}>Men{menu === "mens"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("womens")}}>Women{menu === "womens"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("kids")}}>Kids{menu === "kids"?<hr/>:<></>}</li>
         </ul>
         <div className='nav-login-cart'>
             <button>Login</button>
