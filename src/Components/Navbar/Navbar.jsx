a3abafae23575f454b096f0970d69cd31e4e79d3
ChaiwatAikaew <chaiwatnba@gmail.com>
2025-09-29 23:58:24 -0400

    Add components to first page and separate pages into sections

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -14,14 +14,14 @@ const Navbar = () => {
 
         </div>
         <ul className='nav-menu'>
-            <li onClick={()=>{setMenu("shop")}}><Link to="">Shop</Link>{menu === "shop"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("mens")}}>Men{menu === "mens"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("womens")}}>Women{menu === "womens"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("kids")}}>Kids{menu === "kids"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("shop")}}><Link style={{ textDecoration: 'none' }} to="/">Shop</Link>{menu === "shop"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("mens")}}><Link style={{ textDecoration: 'none' }} to="/mens">Men</Link>{menu === "mens"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("womens")}}><Link style={{ textDecoration: 'none' }} to="/womens">Women</Link>{menu === "womens"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("kids")}}><Link style={{ textDecoration: 'none' }} to="/kids">Kids</Link>{menu === "kids"?<hr/>:<></>}</li>
         </ul>
         <div className='nav-login-cart'>
-            <button>Login</button>
-            <img src={cart_icon} alt=""/>
+            <Link to="/login"><button>Login</button></Link>
+            <Link to="/cart"><img src={cart_icon} alt=""/></Link>
             <div className='nav-cart-count'>0</div>
         </div>
     </div>
