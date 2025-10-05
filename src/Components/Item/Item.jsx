bec7c8b48de599c4ffcc2050213bbe427465fe2b
ChaiwatAikaew <chaiwatnba@gmail.com>
2025-10-04 21:47:33 -0400

    Add Breadcrums and ProductDisplay components, update style and product page

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Item.css'
+import { Link } from 'react-router-dom'
 
 const Item = (props) => {
   return (
     <div className='item'>
-      <img src={props.image} alt='' />
-      <p>{props.name}</p>
+      <Link to={`/product/${props.id}`}><img src={props.image} alt='' /></Link>
       <div className="item-prices">
         <div className="item-price-new">
             ${props.new_price}
