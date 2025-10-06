00f8624790a36c94e33926ef369c3362656e3e2e
ChaiwatAikaew <chaiwatnba@gmail.com>
2025-10-06 18:27:06 -0400

    Update UI styles for DescriptionBox and Product components

--- src/Components/DescriptionBox/DescriptionBox.jsx
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -4,7 +4,20 @@ import './DescriptionBox.css'
 const DescriptionBox = () => {
   return (
     <div className='descriptionbox'>
-      
+      <div className="descriptionbox-nagigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>An e-commerce website is an online platform that facilitate the 
+          buying and selling of products or services over the internet. It 
+          serves as a vitual marketplace where businesses and individuals can 
+          showcase their product, interact with customers, and conduct transactions without the need for a physical presence.
+          E-commerce website have gained immense popularity due to their convenien</p>
+          <p>
+
+          </p>
+      </div>
     </div>
   )
 }
