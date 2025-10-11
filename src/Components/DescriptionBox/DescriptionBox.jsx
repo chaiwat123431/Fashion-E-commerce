a37e7e4432cb6917acfd95f9f14206c404646198
ChaiwatAikaew <chaiwatnba@gmail.com>
2025-10-10 20:00:00 -0400

    Add RelatedProducts component and improve product description layout

--- src/Components/DescriptionBox/DescriptionBox.jsx
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -12,10 +12,14 @@ const DescriptionBox = () => {
         <p>An e-commerce website is an online platform that facilitate the 
           buying and selling of products or services over the internet. It 
           serves as a vitual marketplace where businesses and individuals can 
-          showcase their product, interact with customers, and conduct transactions without the need for a physical presence.
-          E-commerce website have gained immense popularity due to their convenien</p>
+          showcase their product, interact with customers, and conduct 
+          transactions without the need for a physical presence. E-commerce 
+          website have gained immense popularity due to their convenience 
+          accessibility, and the global reach they offer.</p>
           <p>
-
+          E-commerce websites typically display products or services along with 
+          detailed descriptions, images, prices, and any available variations (e.g., sizes, colors). Each product usually has its own dedicated page 
+          with relevant information.
           </p>
       </div>
     </div>
