1482a0ce916b4842d354abb76d6394bf27872f0c
ChaiwatAikaew <chaiwatnba@gmail.com>
2025-10-18 21:00:00 -0400

    Initialize full-stack structure (frontend, backend, admin)

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+// Database Connection With MongoDB
+mongoose.connect(
+  "mongodb+srv://chaiwatnba_db_user:aon54011310553@cluster0.xodqwlo.mongodb.net/e-commerce"
+);
+
+// API Creation
+
+app.listen(port, (error) => {
+  if (!error) {
+    console.log("Server Running on Port " + port);
+  } else {
+    console.log("Error : " + error);
+  }
+});
